8528f8f790a3b53cda79180b2dc19de01e97413e
khris <khris56@hotmail.com>
2019-08-12 11:34:13 -0500

    react routes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import {Router, Route, hashHistory, IndexRedirect} from 'react-router'
 import {App, Page1, Page2, NotFound} from './App';
 import './index.css';
 
-
-// Implementa las rutas aca
-
 ReactDOM.render(
-  <App />, // Es posible que tengas que hacer un cambio en esta linea.
+  <App >
+  <Router history={hashHistory}>
+    <Route path="/" component={Page1} />
+    <Route path="/1" component={Page1} />
+    <Route path="/2" component={Page2} />
+    <Route path="/*" component={NotFound} />
+  </Router>
+
+  </App>,
   document.getElementById('root')
 );
